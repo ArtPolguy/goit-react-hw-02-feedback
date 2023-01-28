f08268289c267a310266cbf3ac5c886384e24f2a
Artur Polguy <art.polguy@gmail.com>
2023-01-28 21:14:17 +0200

    update components feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -5,10 +5,14 @@ import css from './Feedback.module.css';
 class Feedback extends Component {
   static propTypes = {};
 
+  static defaultProps = {
+    initialValue: 0,
+  };
+
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    good: this.props.initialValue,
+    neutral: this.props.initialValue,
+    bad: this.props.initialValue,
   };
 
   handleBtnClick = e => {
@@ -25,9 +29,12 @@ class Feedback extends Component {
   };
 
   countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const posPercentFeeds = (good * 100) / total;
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    if (!total) {
+      return 0;
+    }
+    const posPercentFeeds = Math.round((good * 100) / total);
     return posPercentFeeds;
   };
 
@@ -61,9 +68,9 @@ class Feedback extends Component {
         <p className={css.statsValue}>Good: {good}</p>
         <p className={css.statsValue}>Neutral: {neutral}</p>
         <p className={css.statsValue}>Bad: {bad}</p>
-        <p className={css.statsValue}>Total:{this.countTotalFeedback()}</p>
+        <p className={css.statsValue}>Total: {this.countTotalFeedback()}</p>
         <p className={css.statsValue}>
-          Total:{this.countPositiveFeedbackPercentage()}
+          Total: {this.countPositiveFeedbackPercentage()}%
         </p>
       </div>
     );
